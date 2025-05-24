6d5c1108843ad266b839f89616f387492e4b5969
Arthur <arthurd.rios10@gmail.com>
2025-05-24 18:18:38 -0300

    SearchBar Adicionada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Botao from './components/Botao/Botao'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <SearchBar></SearchBar>
       <Botao></Botao>
       <Footer></Footer>
     </>
---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './SearchBar.css';
+import Botao from '../Botao/Botao'
+
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div className="search-bar-container">
+      <input
+        type="text"
+        placeholder="Buscar livro por título, autor"
+        value={searchTerm}
+        onChange={handleInputChange}
+        className="search-input"
+      />
+      <Botao
+        label="Buscar"
+        cor={true}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
