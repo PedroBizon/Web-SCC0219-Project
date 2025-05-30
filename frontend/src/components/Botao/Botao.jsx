67e021abb5757ae23f2fa91f7de15757335d6bc7
Arthur <arthurd.rios10@gmail.com>
2025-05-30 14:45:15 -0300

    Hover no Botão

--- frontend/src/components/Botao/Botao.jsx
+++ frontend/src/components/Botao/Botao.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Botao.css'
 
 function GerarBotao({ cor , label, onClick, className }) {
-  const colors = ['bg-azul','bg-green-500','bg-red-500'];
+  const colors = ['bg-azul','bg-green-500','bg-[#ab2e2e]'];
   return (
     <button
       onClick={onClick}
