f48450405f816594ce52b52cdac2dcec94c168f6
Arthur <arthurd.rios10@gmail.com>
2025-06-21 15:51:23 -0300

    backend instalado

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Conectar ao MongoDB local
+mongoose.connect('mongodb://localhost:27017/meusite', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.error("Erro ao conectar no MongoDB:", err));
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send("Backend funcionando!");
+});
+
+app.listen(3001, () => {
+  console.log('Servidor rodando em http://localhost:3001');
+});
