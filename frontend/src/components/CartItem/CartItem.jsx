2155662ccb1dec04d06946775597e0da1d0b4541
Arthur <arthurd.rios10@gmail.com>
2025-06-27 01:13:20 -0300

    imagem aparecendo carrinho

--- frontend/src/components/CartItem/CartItem.jsx
+++ frontend/src/components/CartItem/CartItem.jsx
@@ -5,7 +5,7 @@ function CartItem({ book, onRemove }) {
   return (
     <div className="cart-item">
       <div className="book-image">
-        <img src={book.foto} alt={book.nome} />
+        <img src={book.imagem || '/imagens/default.jpg'} alt={book.nome} />
       </div>
       
       <div className="book-info">
